6e547a15bf3b67459c2415305470a13321892c92
cbk702 <cbk702@naver.com>
2022-05-16 22:57:43 +0900

    11

---
+++ app/src/main/java/com/example/diet/EditProfile.java
@@ -0,0 +1,12 @@
+package com.example.diet;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class EditProfile extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.editprofile);
+    }
+}
---
+++ app/src/main/java/com/example/diet/SignUp.java
@@ -0,0 +1,38 @@
+package com.example.diet;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SignUp extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.signup);
+    }
+
+    final String[] SP_index = {"활동량을 선택하십시오", "활동량 적음(25)", "활동량 보통(35)", "활동량 많음(40)"};
+
+    Spinner spinner = (Spinner)findViewById(R.id.spinner1);
+
+    ArrayAdapter adapter = new ArrayAdapter(this, android.R.layout.simple_spinner_item, SP_index);
+    /*adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+    spinner.setAdapter(adapter);
+
+    spiner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        @Override
+        public void onItemSelected(AdapterView<?> adapterView, View view, int position, long id) {
+            Toast.makeText(getApplicationContext(), "Selected Country: "+categories[position] , Toast.LENGTH_SHORT).show();
+        }
+
+        @Override
+        public void onNothingSelected(AdapterView<?> adapterView) {
+
+        }
+    });*/
+}
